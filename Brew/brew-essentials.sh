97dc676e2ddd3d86005c0d32005cc37499356ad2
ReevesA1 <adamrivard87@gmail.com>
2025-08-29 14:53:47 -0400

    d

--- Brew/brew-essentials.sh
+++ Brew/brew-essentials.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+#* Command line tools
 brew install fastfetch
 brew install just
 brew install go-task
+
+#* Casks
+brew install --cask base #App to create, design, edit and browse SQLite 3 database files
