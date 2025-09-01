7418470926838dd8a66ed7bd5a958a5ef07505d3
ReevesA1 <adamrivard87@gmail.com>
2025-09-01 19:31:58 -0400

    s

--- Brew/brew-essentials.sh
+++ Brew/brew-essentials.sh
@@ -9,3 +9,100 @@ brew install go-task
 brew install --cask base #App to create, design, edit and browse SQLite 3 database files
 brew install --cask startupfolder
 brew install --cask quitter
+
+
+
+###########################################
+### Brews - Command Line Core Utilities ###
+###########################################
+brew install git
+brew install gh
+brew install jq
+brew install bpytop
+brew install curl
+brew install wget
+brew install fd
+brew install figlet
+brew install fzf
+brew install glow
+brew install gum
+brew install zenity
+brew install eza
+brew install fastfetch
+
+#############
+### Casks ###
+#############
+# Essentials
+brew install --cask ente-auth
+brew install --cask bitwarden
+brew install --cask iterm2
+brew install --cask powershell
+brew install --cask macupdater
+brew install --cask applite
+brew install --cask appcleaner
+brew install --cask pearcleaner
+brew install --cask jordanbaird-ice
+brew install --cask firefox
+brew install --cask iina
+brew install --cask the-unarchiver
+brew install --cask visual-studio-code
+brew install --cask localsend
+brew install --cask orbstack #installs docker and docker-compose
+brew install --cask stats
+brew install --cask quitter
+brew install --cask startupfolder
+
+################
+### Optional ###
+################
+brew install neovim
+brew install helix
+brew install --cask protonvpn
+brew install --cask proton-mail
+brew install --cask notion
+brew install --cask todoist
+brew install --cask craft
+brew install --cask standard-notes
+brew install --cask messenger
+brew install --cask spotify
+brew install --cask chatgpt
+brew install --cask ollama-app
+brew install --cask brave-browser
+brew install --cask mullvad-browser
+brew install --cask raycast
+brew install --cask tailscale-app
+brew install --cask zerotier-one
+brew install --cask logi-options-plus
+brew install --cask signal
+brew install --cask virtualbuddy
+brew install --cask utm
+brew install --cask container
+brew install --cask google-drive
+brew install --cask megasync
+brew install --cask exifcleaner
+brew install --cask angry-ip-scanner
+brew install --cask jdownloader
+brew install --cask qbittorrent
+brew install --cask base #App to create, design, edit and browse SQLite 3 database files
+
+
+#VM's
+brew install --cask parallels
+brew install --cask utm
+
+#Out Going Firewalls (Choose one)
+brew install --cask lulu
+brew install --cask little-snitch
+
+
+#############
+###  Mas  ###
+#############
+mas install 1136220934 # Infuse
+mas install 1284863847 #unsplash wallpaper now use https://www.wallper.app/
+mas install 6447125648 #Gestimer
+mas install 897118787 #Shazam
+mas install 1355679052 #dropover bought it $$$
+mas install 937984704 #Amphetamine
+mas install 6745342698 #ublock origine lite
