92970340d913e67dc8c81b66edd67c015546bec3
ReevesA1 <adamrivard87@gmail.com>
2025-09-08 12:08:47 -0400

    s

--- Brew/brew-clitools.sh
+++ Brew/brew-clitools.sh
@@ -2,22 +2,24 @@
 
 
 gum choose --no-limit --height 20 \
-  "brew install git                          # version control" \
-  "brew install gh                           # GitHub CLI" \
-  "brew install jq                           # JSON processor" \
   "brew install bpytop                       # system monitor" \
   "brew install curl                         # data transfer" \
-  "brew install wget                         # network downloader" \
+  "brew install eza                          # modern ls" \
+  "brew install fastfetch                    # system info" \
   "brew install fd                           # faster find" \
   "brew install figlet                       # ASCII banners" \
   "brew install fzf                          # fuzzy finder" \
+  "brew install gh                           # GitHub CLI" \
+  "brew install git                          # version control" \
   "brew install glow                         # markdown viewer" \
-  "brew install gum                          # terminal UI components" \
-  "brew install zenity                       # GTK dialogs" \
-  "brew install eza                          # modern ls" \
-  "brew install fastfetch                    # system info" \
-  "brew install just                         # command runner / simpler makefile alternative" \
   "brew install go-task                      # task runner / simpler makefile alternative" \
-  "brew install neovim                       # modal text editor" \
+  "brew install gum                          # terminal UI components" \
   "brew install helix                        # Rust text editor" \
+  "brew install jq                           # JSON processor" \
+  "brew install just                         # command runner / simpler makefile alternative" \
+  "brew install neovim                       # modal text editor" \
+  "brew install rclone                       # cloud storage sync" \
+  "brew install rsync                        # file synchronization" \
+  "brew install wget                         # network downloader" \
+  "brew install zenity                       # GTK dialogs" \
 | sh
