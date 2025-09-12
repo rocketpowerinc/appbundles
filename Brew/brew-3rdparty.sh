bb54b0b14056d7733861a6ce8ba312ba6b654834
ReevesA1 <adamrivard87@gmail.com>
2025-09-12 19:48:34 -0400

    s

--- Brew/brew-3rdparty.sh
+++ Brew/brew-3rdparty.sh
@@ -7,17 +7,16 @@ installed=$(brew list --cask 2>/dev/null | tr '\n' '|')
 selected_opts=""
 for app in ente-auth bitwarden macupdater appcleaner the-unarchiver spotify chatgpt messenger iterm2; do
   if echo "$installed" | grep -q "$app"; then
-    selected_opts="$selected_opts --selected=\"brew install --cask $app       # "
     case $app in
-      ente-auth) selected_opts="${selected_opts}encrypted photos\"" ;;
-      bitwarden) selected_opts="${selected_opts}password manager\"" ;;
-      macupdater) selected_opts="${selected_opts}app updater\"" ;;
-      appcleaner) selected_opts="${selected_opts}app uninstaller\"" ;;
-      the-unarchiver) selected_opts="${selected_opts}alternative: Keka\"" ;;
-      spotify) selected_opts="${selected_opts}music\"" ;;
-      chatgpt) selected_opts="${selected_opts}AI assistant\"" ;;
-      messenger) selected_opts="${selected_opts}Facebook chat\"" ;;
-      iterm2) selected_opts="${selected_opts}terminal\"" ;;
+      ente-auth) selected_opts="$selected_opts --selected=\"brew install --cask ente-auth       # encrypted photos\"" ;;
+      bitwarden) selected_opts="$selected_opts --selected=\"brew install --cask bitwarden       # password manager\"" ;;
+      macupdater) selected_opts="$selected_opts --selected=\"brew install --cask macupdater      # app updater\"" ;;
+      appcleaner) selected_opts="$selected_opts --selected=\"brew install --cask appcleaner      # app uninstaller\"" ;;
+      the-unarchiver) selected_opts="$selected_opts --selected=\"brew install --cask the-unarchiver  # alternative: Keka\"" ;;
+      spotify) selected_opts="$selected_opts --selected=\"brew install --cask spotify         # music\"" ;;
+      chatgpt) selected_opts="$selected_opts --selected=\"brew install --cask chatgpt         # AI assistant\"" ;;
+      messenger) selected_opts="$selected_opts --selected=\"brew install --cask messenger       # Facebook chat\"" ;;
+      iterm2) selected_opts="$selected_opts --selected=\"brew install --cask iterm2          # terminal\"" ;;
     esac
   fi
 done
